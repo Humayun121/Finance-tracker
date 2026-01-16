a494d7b32ba67dd772fae006789cbe7d66fd9616
Humayun121 <Humayun121@users.noreply.github.com>
2026-01-16 00:11:13 +0000

    added POST method for category API

--- backend/expenses/views.py
+++ backend/expenses/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Category
 from .serializers import CategorySerializer
 
-@api_view(["GET"])
+@api_view(["GET","POST"])
 def category_list(request):
     """
-    Seralises the data from the database query and returns it into JSON
+    List all categories or create a new category
     """
-    categories = Category.objects.all()
-    serializer = CategorySerializer(categories, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+    
+    if request.method == "POST":
+        serializer = CategorySerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
