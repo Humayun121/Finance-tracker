fa548dae037bbcb83bc29aaee8c8732991feb8c1
Humayun121 <Humayun121@users.noreply.github.com>
2026-08-27 22:22:17 +0100

    Add category listing and creation

--- frontend/src/api/categories.ts
+++ frontend/src/api/categories.ts
@@ -5,3 +5,12 @@ export async function getCategories(): Promise<Category[]> {
   const res = await apiFetch('/api/categories/');
   return res.json();
 }
+
+export async function createCategory(name: string): Promise<Category> {
+  const res = await apiFetch('/api/categories/', {
+    method: 'POST',
+    body: JSON.stringify({ name }),
+  });
+
+  return res.json();
+}
--- frontend/src/pages/CategoriesPage.tsx
+++ frontend/src/pages/CategoriesPage.tsx
@@ -1,11 +1,53 @@
 import { Nav } from '../components/layout/Nav';
+import type { Category } from '../types/models';
+import { createCategory, getCategories } from '../api/categories';
+import { useEffect, useState } from 'react';
 
 export function CategoriesPage() {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [newCategory, setNewCategory] = useState('');
+
+  useEffect(() => {
+    async function fetchCategories() {
+      const data = await getCategories();
+      setCategories(data);
+    }
+    fetchCategories();
+  }, []);
+
+  async function handleAddCategory() {
+    const trimmedCategory = newCategory.trim()
+
+    if (!trimmedCategory) {
+      return;
+    }
+
+    const createdCategory = await createCategory(trimmedCategory);
+
+    setCategories([...categories, createdCategory]);
+    setNewCategory('');
+  }
+
   return (
     <div>
       <Nav />
+
       <div style={{ padding: 'var(--space-8)' }}>
         <h1>Categories</h1>
+        <input
+        type="text"
+        value={newCategory}
+        onChange={(event) => setNewCategory(event.target.value)}
+        />
+      <button onClick={handleAddCategory}>
+      Add Category
+      </button>
+        
+
+        {categories.map((category) => (
+          <p key={category.id}>{category.name}</p>
+        ))}
+
       </div>
     </div>
   );
